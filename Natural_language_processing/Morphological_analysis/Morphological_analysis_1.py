430e04e6621c3929a0c343f9c50caad95995c181
aqa29 <groupV1111@gmail.com>
2021-11-22 22:37:19 +0900

    natural language processing

---
+++ Natural_language_processing/Morphological_analysis/Morphological_analysis_1.py
@@ -0,0 +1,49 @@
+﻿#Pandasをimport
+import pandas as pd 
+import Stop_word
+
+#csvファイルをData Frameに読み込む
+df = pd.read_csv("gamble_analysis.csv")
+
+#表示
+print(df)
+
+# sentence_list = []
+
+# # extract the data in the second column
+# df = csvfile_open(file)
+# for synopsis in range(len(df)):
+#     sentence_list.append(df[synopsis][1])
+
+#MeCab等、必要なパッケージをimport
+import MeCab
+import re
+
+#MeCabのインスタンスを作成する
+mecab = MeCab.Tagger('')
+
+#最終的な結果を出力するためのData Frameを作成する
+df1 = pd.DataFrame( columns=['タイトル','word0','word1','word2','word3','word4','word5','word6'] )
+
+#dfにセットしたcsvファイルを一行ずつ読み込む
+for row, item in df.iterrows():
+    #形態素解析結果をセットする変数。一行ずつ（opinionaireid単位）処理するため、処理前に一度クリアする
+    result = ''
+    #一行ずつ形態素解析を行い、単語ごとに分ける
+    result = mecab.parse(item.bookliststory)
+    #一行ごとに分けて変数にセットする
+    lines = result.split("\n")
+    #後ろから２列は不要のため削除する
+    lines = lines[0:-2]
+    #一行ずつに分けた形態素解析結果の変数を上から順に読み込む
+    for words in lines:
+        #タブとカンマで区切られているため、配列して新たな変数にセットする
+        word = re.split('\t|,',words)
+        #結果をData Frameにセットする
+        df1 = df1.append({'タイトル':item, 'word0':word[0], 'word1':word[1], 'word2':word[2], 'word3':word[3], 'word4':word[4], 'word5':word[5], 'word6':word[6]},ignore_index=True)
+
+
+#全件表示
+pd.set_option('display.max_rows', None)
+#Data Frameの中身を表示する
+print(df1)
---
+++ Natural_language_processing/Morphological_analysis/Stop_word.py
@@ -0,0 +1,61 @@
+﻿# def stopwords(self, doc):
+#     """
+#     Get stopwords from input document.
+#     """
+#     import nltk
+#     # Judged by class
+#     word_class = self.word_and_class(doc)        
+#     ok_class = [u"名詞", u"動詞"]
+#     stopwords = []
+#     for wc in word_class:
+#         if not wc[1] in ok_class:
+#             stopwords.append(wc[0])
+
+#     # Defined by SlpothLib
+#     slothlib_path = 'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt'
+#     slothlib_file = urllib2.urlopen(slothlib_path)
+#     slothlib_stopwords = [line.decode("utf-8").strip() for line in slothlib_file]
+#     slothlib_stopwords = [ss for ss in slothlib_stopwords if not ss==u'']
+
+#     # Merge and drop duplication
+#     stopwords += slothlib_stopwords
+#     stopwords = list(set(stopwords))
+
+#     return stopwords
+
+# text = '日本語の自然言語処理は本当にしんどい、と彼は十回言った。'
+# sw = stopwords(text)
+
+# words = nltk.word_tokenize(text)
+# print('分かち書き：')
+# for w in words:
+#     print(w)
+
+# print('分かち書き(ストップワードを除去)：')
+# for w in words:
+#     if not w in sw:
+#        print(w)
+
+import os
+import urllib.request
+
+def download_stopwords(path):
+    url = 'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt'
+    if os.path.exists(path):
+        print('File already exists.')
+    else:
+        print('Downloading...')
+        # Download the file from `url` and save it locally under `file_name`:
+        urllib.request.urlretrieve(url, path)
+
+def create_stopwords(file_path):
+    stop_words = []
+    for w in open(path, "r"):
+        w = w.replace('\n','')
+        if len(w) > 0:
+          stop_words.append(w)
+    return stop_words    
+
+path = "stop_words.txt"
+download_stopwords(path)
+stop_words = create_stopwords(path)
